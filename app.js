a3da6c8292366030e02bf5967a13bc8817a2f90e
JuanLicciardi <juanelicciardi@gmail.com>
2022-06-28 23:03:21 -0300

    Se crea estructura de carpetas e incorporacion de imagenes

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
+// Declaración de Constantes
 const express = require('express');
 const app = express();
 const path = require ('path');
 const port = 3030;
 
-app.get('/', (req, res) => res.send("ABRIENDO PAGINA"));
 
-app.listen(puerto, () => console.log(`Server Runing in port ${port}`))
+const pathPublic = path.resolve(__dirname,"./public");
+app.use(express.static(pathPublic));
+
+//ejecuta pagina index.html
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,"./views","index.html")));
+
+
+// Activación de Servidor en puerto 3030
+app.listen(port, () => console.log(`Server Runing in port ${port}`))
