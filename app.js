fd7eab7964ec1272223fd0d3fac3fcf7bb52e0cc
JuanLicciardi <juanelicciardi@gmail.com>
2022-07-20 01:32:39 -0300

    Retocando presentación de FlexBox

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.static(pathPublic));
 
 //ejecuta pagina index.html
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,"./views","index.html")));
+app.get('/crearCuenta', (req, res) => res.sendFile(path.join(__dirname,"./views","crearCuenta.html")));
+app.get('/Registrar', (req, res) => res.sendFile(path.join(__dirname,"./views","Registrar.html")));
 
 
 // Activación de Servidor en puerto 3030
